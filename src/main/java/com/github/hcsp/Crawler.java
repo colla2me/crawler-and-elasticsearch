8b2f5ddcb451f7ce14465bd1e12a6a046ebd7c48
samuel <zhangleven@gmail.com>
2020-04-11 05:23:31 +0800

    complete basic crawler

--- src/main/java/com/github/hcsp/Crawler.java
+++ src/main/java/com/github/hcsp/Crawler.java
@@ -6,25 +6,72 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Crawler extends Thread {
 
-    public static void main(String[] args) throws IOException {
-        String userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36";
-        String link = "https://sina.cn";
+    private static final String USERAGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36";
+    private static final String INDEX = "https://sina.cn";
+    private static final String NEWS = "https://news.sina.cn";
 
-        CloseableHttpClient httpclient = HttpClients.custom().setUserAgent(userAgent).build();
-        try (CloseableHttpResponse response = httpclient.execute(new HttpGet(link))) {
-            HttpEntity entity = response.getEntity();
-            String html = EntityUtils.toString(entity);
-            System.out.println("html = " + html);
+    public static void main(String[] args) throws IOException {
+
+        Set<String> visitedLinks = new HashSet<>();
+        Deque<String> unvisitedLinks = new ArrayDeque<>();
+        unvisitedLinks.add(INDEX);
+
+        CloseableHttpClient httpClient = HttpClients.custom().setUserAgent(USERAGENT).build();
+        String link;
+        while ((link = unvisitedLinks.poll()) != null) {
+            if (visitedLinks.contains(link)) continue;
+
+            try (CloseableHttpResponse response = httpClient.execute(new HttpGet(link))) {
+                HttpEntity entity = response.getEntity();
+                String html = EntityUtils.toString(entity);
+
+                Document document = Jsoup.parse(html);
+                Elements articleTags = document.select("article");
+                if (!articleTags.isEmpty()) {
+                    String title = articleTags.get(0).child(0).text();
+                    System.out.println("title = " + title);
+                }
+
+                Elements aTags = document.select("a");
+                for (Element aTag : aTags) {
+                    String href = aTag.attr("href");
+                    if (href.startsWith("//")) {
+                        href = "https:" + href;
+                    }
+
+                    if (!isValidateLink(href)) {
+                        continue;
+                    }
+
+                    unvisitedLinks.add(href);
+                    System.out.println("href = " + href);
+                }
+
+                visitedLinks.add(link);
+            }
         }
     }
 
-    @Override
-    public void run() {
-
+    private static boolean isValidateLink(String link) {
+        if (INDEX.equals(link) || link.startsWith(NEWS)) {
+            return true;
+        }
+        return false;
     }
+
+    @Override
+    public void run() { }
 }
