335031ca3fcfb224e14f783d94ecae98a24ae5ee
samuel <zhangleven@gmail.com>
2020-04-11 07:36:57 +0800

    using h2database to store

--- src/main/java/com/github/hcsp/Crawler.java
+++ src/main/java/com/github/hcsp/Crawler.java
@@ -11,10 +11,11 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.Set;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Crawler extends Thread {
@@ -22,9 +23,6 @@ public class Crawler extends Thread {
     private static final String USERAGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36";
     private static final String INDEX = "https://sina.cn";
     private static final String NEWS = "https://news.sina.cn";
-
-    private Set<String> visitedLinks = new HashSet<>();
-    private Deque<String> unvisitedLinks = new ArrayDeque<>();
 
     public static void main(String[] args) {
         new Crawler().start();
@@ -58,7 +56,7 @@ public class Crawler extends Thread {
         }
     }
 
-    private void parseUrlsFromPageAndStoreIntoDatabase(Document document) {
+    private void parseUrlsFromPageAndStoreIntoDatabase(Document document, Connection connection) {
         Elements aTags = document.select("a");
         for (Element aTag : aTags) {
             String href = aTag.attr("href");
@@ -70,30 +68,90 @@ public class Crawler extends Thread {
                 continue;
             }
 
-            unvisitedLinks.add(href);
+            insertLinkIntoUnvisited(connection, href);
         }
     }
 
-    private void startCrawling() {
+    private void startCrawling(Connection connection) {
         CloseableHttpClient httpClient = HttpClients.custom()
-                                                    .setUserAgent(USERAGENT)
-                                                    .build();
-        String link;
-        while ((link = unvisitedLinks.poll()) != null) {
-            if (visitedLinks.contains(link)) {
+                .setUserAgent(USERAGENT)
+                .build();
+        while (true) {
+            List<String> unvisitedLinks = loadUrlsFromDatabase(connection, "select link from UNVISITED_LINKS");
+            if (unvisitedLinks.isEmpty()) {
+                break;
+            }
+
+            String link = unvisitedLinks.remove(unvisitedLinks.size() - 1);
+            deleteLinkFromDatabase(connection, link);
+
+            if (isVisitedLink(connection, link)) {
                 continue;
             }
 
             Document document = httpGetAndParse(link, httpClient);
-            parseUrlsFromPageAndStoreIntoDatabase(document);
+            parseUrlsFromPageAndStoreIntoDatabase(document, connection);
             storeOnlyNewsIntoDatabase(document, link);
-            visitedLinks.add(link);
+            insertLinkIntoVisited(connection, link);
+        }
+    }
+
+    private List<String> loadUrlsFromDatabase(Connection connection, String sql) {
+        List<String> results = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                results.add(resultSet.getString(1));
+            }
+            return results;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void deleteLinkFromDatabase(Connection connection, String link) {
+        try (PreparedStatement statement = connection.prepareStatement("delete from UNVISITED_LINKS where link = ?")) {
+            statement.setString(1, link);
+            statement.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void insertLinkIntoUnvisited(Connection connection, String link) {
+        try (PreparedStatement statement = connection.prepareStatement("insert into UNVISITED_LINKS (link) values (?)")) {
+            statement.setString(1, link);
+            statement.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void insertLinkIntoVisited(Connection connection, String link) {
+        try (PreparedStatement statement = connection.prepareStatement("insert into VISITED_LINKS (link) values (?)")) {
+            statement.setString(1, link);
+            statement.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean isVisitedLink(Connection connection, String link) {
+        try (PreparedStatement statement = connection.prepareStatement("select link from VISITED_LINKS where link = ?")) {
+            statement.setString(1, link);
+            ResultSet resultSet = statement.executeQuery();
+            return resultSet.next();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
     @Override
     public void run() {
-        unvisitedLinks.add(INDEX);
-        startCrawling();
+        try (Connection connection = DriverManager.getConnection("jdbc:h2:file:/Users/samuel/IdeaProjects/crawler-and-elasticsearch/news", "root", "root")) {
+            startCrawling(connection);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
