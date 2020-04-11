94205578ec612b1baedabdefa1cfee601fd16790
samuel <zhangleven@gmail.com>
2020-04-12 01:46:21 +0800

    create index for table news

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -3,7 +3,7 @@ package com.github.hcsp;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 3; i++) {
             new Crawler(new MyBatisCrawlerDao()).start();
         }
     }
